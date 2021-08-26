9ae45c8fecc4f8b782a0fc5083e4385b899597b5
vladimir jelea <jeleavladimir@yahoo.ro>
2021-08-26 11:25:14 +0300

    Initial commit

---
+++ src/test/java/com/playtika/gamesettingsapi/GameSettingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playtika.gamesettingsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSettingsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
