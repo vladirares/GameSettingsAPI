74ad13045123f8341938b1bf448ad3618c818642
vladimir jelea <jeleavladimir@yahoo.ro>
2021-08-26 15:47:48 +0300

    Role model for future security

--- src/main/java/com/playtika/gamesettingsapi/models/User.java
+++ src/main/java/com/playtika/gamesettingsapi/models/User.java
@@ -1,10 +1,12 @@
 package com.playtika.gamesettingsapi.models;
 
+import com.playtika.gamesettingsapi.security.Role;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
     @Id
     @Column(name = "id")
@@ -17,15 +19,51 @@ public class User {
     @Column(name = "user_last_name")
     private String lastName;
 
+    @Column(name = "username")
+    private String username;
+
     @Column(name = "user_max_daily_playtime")
     private int maxPlaytime;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 
     @OneToMany(
             mappedBy = "user",
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
+
     private List<GameSession> gameSessions;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public List<GameSession> getGameSessions() {
         return gameSessions;
---
+++ src/main/java/com/playtika/gamesettingsapi/security/Role.java
@@ -0,0 +1,58 @@
+package com.playtika.gamesettingsapi.security;
+
+import com.playtika.gamesettingsapi.models.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String roleType) {
+        this.name = roleType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+}
